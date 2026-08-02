c90bc6fd63b0562b7399ef9d96383b2ee5668cf9
runner <runner@localhost>
2026-08-02 23:41:19 +0200

    Update

---
+++ post/22-09-ios-obfuscation-syscall-hooking/zoom.js
@@ -0,0 +1,11 @@
+var cfg_flat = svgPanZoom('#sub_ebdc', {
+  zoomScaleSensitivity: 0.5,
+  controlIconsEnabled: 1,
+});
+cfg_flat.zoom(1.5)
+
+
+window.addEventListener('load', function () {
+  Lightense('.lightense');
+}, false);
+
